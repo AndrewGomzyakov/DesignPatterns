dde0ed3e0c66142c667dad10c190e6ba4b6d8a4a
Andrew <avural-k@mail.ru>
2019-12-08 20:28:56 +0500

    HW4
    HW4

---
+++ DesignPatterns/Adapter/Clients/IOrmAdapter.cs
@@ -0,0 +1,12 @@
+﻿using Adapter.FirstOrmLibrary;
+using Adapter.Models;
+
+namespace Adapter.Clients
+{
+    public interface IOrmAdapter
+    {
+        (DbUserEntity, DbUserInfoEntity) Get(int userId);
+        void Add(DbUserEntity user, DbUserInfoEntity userInfo);
+        void Remove(int userId);
+    }
+}
---
+++ DesignPatterns/Adapter/Clients/UserClient.cs
@@ -0,0 +1,38 @@
+﻿using Adapter.FirstOrmLibrary;
+using Adapter.Models;
+using Example_04.Homework.SecondOrmLibrary;
+
+namespace Adapter.Clients
+{
+    public class UserClient
+    {
+        private IOrmAdapter _ormAdapter;
+
+        private IFirstOrm<DbUserEntity> _firstOrm1;
+        private IFirstOrm<DbUserInfoEntity> _firstOrm2;
+
+        private ISecondOrm _secondOrm;
+
+        private bool _useFirstOrm = true;
+
+        public (DbUserEntity, DbUserInfoEntity) Get(int userId)
+        {
+            return _ormAdapter.Get(userId);
+        }
+
+        public void Add(DbUserEntity user, DbUserInfoEntity userInfo)
+        {
+            _ormAdapter.Add(user, userInfo);
+        }
+
+        public void Remove(int userId)
+        {
+            _ormAdapter.Remove(userId);
+        }
+
+        public UserClient(IOrmAdapter ormAdapter)
+        {
+            _ormAdapter = ormAdapter;
+        }
+    }
+}
